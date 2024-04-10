9fe2d5cc3fbeb30318faf5811f85f87287628e38
Simone <iannisimo@gmail.com>
2024-04-10 09:59:35 +0200

    del howdy

--- modules/nixos/features/howdy.nix
+++ modules/nixos/features/howdy.nix
@@ -1,24 +1,26 @@
-{
-  inputs,
-  pkgs,
-  config,
-  lib,
-  ...
-}: {
-  imports = [
-    "${inputs.nixpkgs-howdy}/nixos/modules/security/pam.nix"
-    "${inputs.nixpkgs-howdy}/nixos/modules/services/security/howdy/"
-  ];
+{...}: {}
+
+# {
+#   inputs,
+#   pkgs,
+#   config,
+#   lib,
+#   ...
+# }: {
+#   imports = [
+#     "${inputs.nixpkgs-howdy}/nixos/modules/security/pam.nix"
+#     "${inputs.nixpkgs-howdy}/nixos/modules/services/security/howdy/"
+#   ];
     
-  services = {
-    howdy = {
-      enable = true;
-      package = inputs.nixpkgs-howdy.legacyPackages.${pkgs.system}.howdy;
-      settings = {
-        video.device_path = "/dev/video2";
-        core.no_confirmation = true;
-        video.dark_threshold = 90;
-      };
-    };
-  };
-}
+#   services = {
+#     howdy = {
+#       enable = true;
+#       package = inputs.nixpkgs-howdy.legacyPackages.${pkgs.system}.howdy;
+#       settings = {
+#         video.device_path = "/dev/video2";
+#         core.no_confirmation = true;
+#         video.dark_threshold = 90;
+#       };
+#     };
+#   };
+# }

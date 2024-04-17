0c8ac6b3b8d886a5425f88bb353424049ee56d39
Simone <iannisimo@gmail.com>
2024-04-17 12:18:19 +0200

    add vscode extensions

--- modules/nixos/features/code.nix
+++ modules/nixos/features/code.nix
@@ -5,6 +5,34 @@
   ...
 }: {
   environment.systemPackages = with pkgs; [
-    pkgs.vscode.fhsWithPackages (ps: with ps; [ ms-vscode-remote.remote-ssh ])
+    (vscode-with-extensions.override {
+      vscodeExtensions = with vscode-extensions; [
+        
+        bbenoist.nix
+        arrterian.nix-env-selector
+        mkhl.direnv
+
+        github.copilot
+        github.copilot-chat
+        
+        ms-vscode-remote.remote-ssh
+        ms-vscode-remote.remote-containers
+
+        ms-toolsai.jupyter
+        ms-toolsai.vscode-jupyter-slideshow
+        ms-toolsai.vscode-jupyter-cell-tags
+        ms-toolsai.jupyter-renderers
+        ms-python.python
+        ms-python.vscode-pylance
+        
+        ms-azuretools.vscode-docker
+        mechatroner.rainbow-csv
+
+      ]
+    })
   ];
 }
+
+# MISSING
+# ms-python.debugpy
+# ms-toolsai.jupyter-keymap

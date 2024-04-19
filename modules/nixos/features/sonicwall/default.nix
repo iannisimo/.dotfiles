9dce0bbef6ba75c1dfd0ba5cf3b05aadf622b611
Simone <iannisimo@gmail.com>
2024-04-19 16:02:24 +0200

    test

--- modules/nixos/features/sonicwall/default.nix
+++ modules/nixos/features/sonicwall/default.nix
@@ -4,4 +4,10 @@
   config,
   ...
 }: {
+  security.wrappers."AvConnect" = {
+    setuid = true;
+    owner = "root";
+    group = "root";
+    source = "${( pkgs.callPackage ./package.nix { } )}/usr/local/Aventail/AvConnect";
+  };
 }

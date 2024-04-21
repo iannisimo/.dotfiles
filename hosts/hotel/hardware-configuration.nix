c3ab7dfd05e72f50ba1322079e956fe1b61952a4
Simone Ianniciello <iannisimo@gmail.com>
2024-04-21 11:25:37 +0000

    add swap [hotel]

--- hosts/hotel/hardware-configuration.nix
+++ hosts/hotel/hardware-configuration.nix
@@ -21,7 +21,12 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices = [ 
+    {
+      device = "/.swapfile";
+      size = 4096;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's

596497920ec27c95c2758f4a305380c9823b1e2e
Simone <iannisimo@gmail.com>
2024-06-06 13:54:31 +0200

    rem some ext

--- modules/nixos/features/code.nix
+++ modules/nixos/features/code.nix
@@ -27,8 +27,7 @@
 
         ms-vscode.cpptools
         ms-vscode.cpptools-extension-pack
-        xaver.clang-format
-        llvm-vs-code-extensions.vscode-clangd
+        ms-vscode.makefile-tools
 
         ms-azuretools.vscode-docker
         mechatroner.rainbow-csv

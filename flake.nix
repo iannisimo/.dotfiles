b4b4d29f4f2722b9cfd16bc1b58fd83486a8fc9d
Simone <iannisimo@gmail.com>
2024-12-18 11:41:39 +0100

    spicetify change repo

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
     };
 
     spicetify-nix = {
-      url = "github:the-argus/spicetify-nix";
+      url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "unstable-nixpkgs";
     };
 

11fb03b2f3068cab1cba97265ffdff7bed539e07
Simone <iannisimo@gmail.com>
2024-12-18 11:39:38 +0100

    spotify

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -16,11 +16,12 @@
   myHM.git.enable = true;
   myHM.hyprland.enable = true;
   myHM.alacritty.enable = true;
-  # myHM.spotify.enable = true;
+  myHM.spotify.enable = true;
   myHM.direnv.enable = true;
   myHM.zen-browser.enable = true;
   myHM.easyeffects.enable = true;
   myHM.discord.enable = true;
+  myHM.livornowifi.enable = true;
 
   home = {
     username = "simone";
--- modules/home-manager/features/livornowifi.nix
+++ modules/home-manager/features/livornowifi.nix
@@ -1,12 +1,11 @@
 {
   pkgs,
-  config,
   ...
 }: let 
-  script = '''
+  script = ''
 #!/usr/bin/env bash
 curl "http://livornowififreecaptive.hs/login" -d "username=freecaptive" -d "password=nagx.U4Uf." >/dev/null 2>&1
-''';
+'';
 in {
   home.packages = [
     ( pkgs.writeShellScriptBin "livornowifi" script )

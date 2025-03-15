631a871e63bc36a52d3020a178eb4c67c36a8e4a
Simone <iannisimo@gmail.com>
2025-03-15 10:20:56 +0100

    psychosis448/lazyvim-nix-template

--- modules/home-manager/features/nvim/default.nix
+++ modules/home-manager/features/nvim/default.nix
@@ -1,57 +1,74 @@
-{ pkgs, ... }:
-let
-  treesitterWithGrammars = (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
-    p.bash
-    p.comment
-    p.css
-    p.dockerfile
-    p.fish
-    p.gitattributes
-    p.gitignore
-    p.go
-    p.gomod
-    p.gowork
-    p.hcl
-    p.javascript
-    p.jq
-    p.json5
-    p.json
-    p.lua
-    p.make
-    p.markdown
-    p.nix
-    p.python
-    p.rust
-    p.toml
-    p.typescript
-    p.vue
-    p.yaml
-  ]));
-in
 {
+  pkgs,
+  config,
+  ...
+}: {
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
   programs.neovim = {
+    defaultEditor = true;
     enable = true;
-    vimAlias = true;
     viAlias = true;
-    coc.enable = false;
-    withNodeJs = true;
-    withPython3 = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      ## lazyvim base
+      LazyVim
+      lazy-nvim
 
-    plugins = [
-      treesitterWithGrammars
+      ## ui
+      snacks-nvim ## required
+
+      ## additional plugins
+      modus-themes-nvim
     ];
 
-    extraPackages = with pkgs; [
-      ripgrep
-      fd
-      gcc
-      cargo
-      lua-language-server
-      stylua
-      rust-analyzer-unwrapped
-      black
-      nodejs_22
-      gh
-    ];
+    extraLuaConfig = ''
+      vim.g.mapleader = " " -- Need to set leader before lazy for correct keybindings
+      require("lazy").setup({
+        spec = {
+          -- add LazyVim and import its plugins
+          { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+          -- import/override with your plugins
+          { import = "plugins" },
+        },
+        dev = {
+          path = "${pkgs.vimUtils.packDir config.programs.neovim.finalPackage.passthru.packpathDirs}/pack/myNeovimPackages/start",
+          patterns = {""}, -- Specify that all of our plugins will use the dev dir. Empty string is a wildcard!
+        },
+        performance = {
+          -- Used for NixOS
+          reset_packpath = false,
+          rtp = {
+            reset = false,
+            disabled_plugins = {
+              "gzip",
+              -- "matchit",
+              -- "matchparen",
+              -- "netrwPlugin",
+              "tarPlugin",
+              "tohtml",
+              "tutor",
+              "zipPlugin",
+            },
+          }
+        },
+        defaults = {
+          -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+          -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+          lazy = false,
+          -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+          -- have outdated releases, which may break your Neovim install.
+          version = false, -- always use the latest git commit
+          -- version = "*", -- try installing the latest stable version for plugins that support semver
+        },
+        install = {
+          -- Safeguard in case we forget to install a plugin with Nix
+          missing = false,
+        },
+      })
+    '';
   };
 }

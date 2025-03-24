4d7ed808ea6e02aeadbd7e4a57e58a507adf5291
Simone <iannisimo@gmail.com>
2025-03-24 09:42:33 +0100

    msi-ec

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -10,8 +10,8 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+  boot.kernelModules = [ "kvm-intel" "msi-ec" ];
+  boot.extraModulePackages = [ config.boot.kernelPackages.msi-ec ];
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/ca0db07e-507f-4350-b9bc-6fa1b8c6754b";

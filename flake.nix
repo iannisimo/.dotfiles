796220cf62b69e2dface91070c15023c4630c403
Simone <iannisimo@gmail.com>
2025-03-26 14:20:09 +0100

    revert zen

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
 
 
     zen-browser = {
-      url = "github:FBIGlowie/zen-browser-flake";
+      url = "github:iannisimo/zen-browser-flake";
       inputs.nixpkgs.follows = "unstable-nixpkgs";
     };
 

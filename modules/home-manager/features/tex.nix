4421d72b667143829a008db018287eb568b02ed0
Simone <iannisimo@gmail.com>
2025-03-26 14:20:16 +0100

    tex libs

--- modules/home-manager/features/tex.nix
+++ modules/home-manager/features/tex.nix
@@ -6,7 +6,7 @@
 let
   tex = (
     pkgs.texlive.combine {
-      inherit (pkgs.texlive) scheme-small latexmk revtex4;
+      inherit (pkgs.texlive) scheme-small latexmk revtex4 mdframed lipsum pgfplots;
     }
   );
 in {

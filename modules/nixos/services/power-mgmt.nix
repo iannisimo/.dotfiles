bb9660eb7b74b533ee2a96c41479faeb79264b4f
Simone <iannisimo@gmail.com>
2025-04-19 20:41:14 +0200

    tlp change

--- modules/nixos/services/power-mgmt.nix
+++ modules/nixos/services/power-mgmt.nix
@@ -14,7 +14,7 @@
           INTEL_GPU_MIN_FREQ_ON_BAT=300;
 
           INTEL_GPU_MAX_FREQ_ON_AC=1150;
-          INTEL_GPU_MAX_FREQ_ON_BAT=700;
+          INTEL_GPU_MAX_FREQ_ON_BAT=300;
           
           INTEL_GPU_BOOST_FREQ_ON_AC=1150;
           INTEL_GPU_BOOST_FREQ_ON_BAT=700;
@@ -22,7 +22,7 @@
           CPU_MIN_PERF_ON_AC = 20;
           CPU_MAX_PERF_ON_AC = 100;
           CPU_MIN_PERF_ON_BAT = 0;
-          CPU_MAX_PERF_ON_BAT = 10;
+          CPU_MAX_PERF_ON_BAT = 50;
 
           CPU_BOOST_ON_AC=1;
           CPU_BOOST_ON_BAT=0;
@@ -33,7 +33,7 @@
           CPU_DRIVER_OPMODE_ON_AC="active";
           CPU_DRIVER_OPMODE_ON_BAT="active";
 
-          CPU_ENERGY_PERF_POLICY_ON_AC="performance";
+          CPU_ENERGY_PERF_POLICY_ON_AC="balance_performance";
           CPU_ENERGY_PERF_POLICY_ON_BAT="power";
 
           CPU_SCALING_GOVERNOR_ON_AC="performance";
